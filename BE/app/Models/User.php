780b9cbc546277d58841a657570f85f736d6527f
Hesam Aghatabay <program.h.p.2023@gmail.com>
2025-03-29 13:57:49 +0330

    update

--- BE/app/Models/User.php
+++ BE/app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'mobile',
         'email',
         'password',
     ];
@@ -31,6 +32,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'created_at',
+        'updated_at',
+        'daleted_at'
     ];
 
     /**
--- FE/src/pages/IndexPage.vue
+++ FE/src/pages/IndexPage.vue
@@ -1,7 +1,8 @@
 <template>
   <q-page class="text-center">
-    <h3> Index Page</h3>
-    <q-btn to="category" color="green-9"> Go To Category Page</q-btn>
+    <h3>Index Page</h3>
+    <q-btn to="category" class="q-mx-sm" color="green-9"> Go To Category Page</q-btn>
+    <q-btn to="register" class="q-mx-sm" color="blue-8"> Go To Register Page</q-btn>
   </q-page>
 </template>
 
